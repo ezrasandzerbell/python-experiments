a77abb8806b72759be5fe468794e708479053de0
ezra sandzer-bell <equilibriumarts@gmail.com>
2019-09-14 21:24:26 -0700

    magic 8 ball in python

---
+++ magic8ball.py
@@ -0,0 +1,5 @@
+import random
+
+print("Welcome to the Magic 8 Ball.")
+question = input("Ask a question and receive your fortune: ")
+print("The answer is " + random.choice(['yes', 'no', 'maybe']))
